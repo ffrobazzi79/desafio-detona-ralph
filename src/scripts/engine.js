86a95cf6066c6c0418102fb2d7e57a9ea8347069
ffrobazzi79 <dev.ffrobazzi@gmail.com>
2024-11-06 22:42:20 -0300

    Feat: new repository for js intro

---
+++ src/scripts/engine.js
@@ -0,0 +1,82 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+    },
+    values: {
+        // value desabilitado para simplificar com "state.actions.timerId"
+        // gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+// contagem do tempo e apresentação do resultado
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    };
+};
+
+// som e volume
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.1;
+    audio.play();
+}
+
+// posição do inimigo
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy")
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+};
+
+// função desabilitada para simplificar com "state.actions.timerId"
+// function moveEnemy() {
+//     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
+// };
+
+// evento de click e score
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
+};
+
+// função para inicializar a aplicação
+
+function initialize() {
+    // moveEnemy();
+    addListenerHitBox();
+};
+
+initialize();
